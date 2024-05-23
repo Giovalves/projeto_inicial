6a8a0b63eb3f4d31d7cc682d8babdac2d7a059e6
Giovanna <giovalves2005@yahoo.com.br>
2024-05-23 07:20:37 -0300

    meu primeiro commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Jornada de dados")
+
+#variaveis
+idade = 34
+nome = "Giovanna"
+
+#Print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#Metodos
+lista_invertida = lista.reverse()
